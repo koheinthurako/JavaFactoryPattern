25c76195d00370c5eef7dd2ddd4c24928bece17b
koheinthurako <captainthura99@gmail.com>
2024-01-13 22:27:44 +0630

    test greeting program

---
+++ src/main/java/com/factoryPattern/EnglishGreeting.java
@@ -0,0 +1,9 @@
+package com.factoryPattern;
+
+public class EnglishGreeting {
+	
+	public void greet() {
+		System.out.println("Hello");
+	}
+
+}
---
+++ src/main/java/com/factoryPattern/MyanmarGreeting.java
@@ -0,0 +1,9 @@
+package com.factoryPattern;
+
+public class MyanmarGreeting {
+	
+	public void greet() {
+		System.out.println("Mingalarpar");
+	}
+
+}
